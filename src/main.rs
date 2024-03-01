f99ece4b6be235993edf6f81a2b5194c86eb9888
Christian Kuhl <christian.kuhl84@gmail.com>
2024-03-01 20:45:48 +0100

    Begin REPL

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result as ReplResult};
+
 mod parser;
-use parser::tokenizer::tokenize;
+use parser::tokenizer::{tokenize_file, tokenize_line};
 use parser::{parse, ParseResult};
 mod debug;
 
 fn main() -> Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        return Err("No input file provided".to_string())
+        run_repl();
+    } else {
+        run_script(&args[1])?
     }
-    let tokens = tokenize(&args[1])?;
-    // for token in tokens.iter() {
-    //     println!("{:}", token);
-    // }
+    Ok(())
+}
+
+fn run_script(filename: &str) -> Result<(), String> {
+    let tokens = tokenize_file(filename)?;
     let statements = parse(&tokens);
     match statements {
         ParseResult::Ok((statements, rest)) => {
@@ -33,5 +39,40 @@ fn main() -> Result<(), String> {
             }
         }
     };
+    Ok(())
+}
+
+fn run_repl() -> ReplResult<()> {
+    // `()` can be used when no completer is required
+    let mut rl = DefaultEditor::new()?;
+    let mut tokens = vec![];
+    #[cfg(feature = "with-file-history")]
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                let tok = tokenize_line(line.as_str());
+                tokens.extend(tok.unwrap());
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    #[cfg(feature = "with-file-history")]
+    rl.save_history("history.txt");
     Ok(())
 }
